4840d33c10de07375e3ec46bbee2c866ba8a6c25
Robinhood <okaforchidubem7@gmail.com>
2023-07-27 14:08:21 +0100

    classwork1

---
+++ items.js
@@ -0,0 +1,52 @@
+export const items = [
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+  {
+    item: "shirt",
+    quantity: 2,
+    discription: "a short sleeve shirt",
+  },
+  {
+    item: "jeans",
+    quantity: 11,
+    discription: "a nice pair of blue jeans",
+  },
+  {
+    item: "ps5",
+    quantity: 1,
+    discription: "playstation 5",
+  },
+  {
+    item: "xbox",
+    quantity: 1,
+    discription: "xbox series x and s",
+  },
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+  {
+    item: "shoe",
+    quantity: 50,
+    discription: "this is a random shoe",
+  },
+];
